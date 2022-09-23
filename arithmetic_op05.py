de7964530319c7289dfc0929c7a86e80dd7bd855
azimabdurasulov <112892181+azimabdurasulov@users.noreply.github.com>
2022-09-23 14:55:19 +0500

    5-topshiriq

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,10 +1,8 @@
 # Create a variable "a"  and assign it the value of the int 72.
-
+a=72
 # Create a variable "b"  and assign it the value of the int 9.
-
+b=9
 # Create a variable called "answer".
 
 # Find the division of a and b then assign to "answer".
-a=72
-b=9
 answer=a/b
